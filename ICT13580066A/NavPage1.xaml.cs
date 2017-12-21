f49c17fde7d5c281dd1554b9d617a8c82c11bd9b
nopompam_556 <nopompam_5566@hotmail.com>
2017-12-21 14:09:17 +0700

    init

---
+++ ICT13580066A/NavPage1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICT13580066A
+{
+    public partial class NavPage1 : ContentPage
+    {
+        public NavPage1()
+        {
+            InitializeComponent();
+
+			InitializeComponent();
+			nextButton.Clicked += NextButton_Clicked;
+		}
+
+		void NextButton_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new NavPage2());
+        }
+    }
+}
